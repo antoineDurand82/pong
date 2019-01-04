2abd027b3d6980152bbe9e078a129a2e8e6b73cd
Antoine.D <antoine.durand@ynov.com>
2019-01-04 10:37:51 +0100

    hitbox bord + mouvement

--- pong.py
+++ pong.py
@@ -1,21 +1,69 @@
-import tkinter as tk       
+from tkinter import *
 
-class Application(tk.Frame):              
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)   
-        self.grid()                       
-        self.createWidgets()
+def movement_balle():
+    global dbx, dby
+    #rebondissement bord map
+    if (main_canvas.coords(balle)[0]>997) or (main_canvas.coords(balle)[2]==2):
+        dbx=-1*dbx
+    if (main_canvas.coords(balle)[1]>600) or (main_canvas.coords(balle)[3]==0):
+        dby=-1*dby
+    #position
+    main_canvas.move(balle,dbx,dby)
+    #loop
+    pong.after(10,movement_balle)
 
-    def createWidgets(self):
-        self.main_canvas = tk.Canvas(self, height=600, width=1000, bg='black')
-        self.main_canvas.grid()
-        self.main_canvas.create_rectangle(10,200,20,270, fill='white', outline='white')
-        self.main_canvas.create_rectangle(990,200,980,270, fill='white', outline='white')
+def movement_raquette():
+    if (main_canvas.coords(raquette_1)[1]<600) or (main_canvas.coords(raquette_1)[3]==0):
+        def haut(event):
+            main_canvas.move(raquette_1, 0, -10)
+        def bas(event):
+            main_canvas.move(raquette_1, 0, 10)
+        main_canvas.bind_all("z", haut)
+        main_canvas.bind_all("s", bas)
+        
+    if (main_canvas.coords(raquette_2)[1]<600) or (main_canvas.coords(raquette_2)[3]==0):
+        def haut2(event):
+            main_canvas.move(raquette_2, 0, -10)
+        def bas2(event):
+            main_canvas.move(raquette_2, 0, 10)
+        main_canvas.bind_all("<Up>", haut2)
+        main_canvas.bind_all("<Down>", bas2)
 
-"""class raquette()
-    pass"""
 
 
-app = Application()                       
-app.master.title('Sample application')    
-app.mainloop()
+
+
+
+
+
+
+pong = Tk()
+
+#fenetre
+main_canvas = Canvas(pong,width=1000, height=600, bg='black')
+main_canvas.grid()
+
+#position raquette 1
+Pos_rX1 = 10
+Pos_rY1 = 250
+#position raquette 2
+Pos_rX2 = 990
+Pos_rY2 = 250
+#raquette
+raquette_1 = main_canvas.create_rectangle(Pos_rX1,Pos_rY1,Pos_rX1+10, Pos_rY1+70, fill='white', outline='white')
+raquette_2 = main_canvas.create_rectangle(Pos_rX2,Pos_rY2,Pos_rX2-10, Pos_rY2+70, fill='white', outline='white')
+#touche mouvement
+
+#position balle
+Pos_bX = 500
+Pos_bY = 300
+#balle
+balle = main_canvas.create_oval(Pos_bX, Pos_bY,Pos_bX+20,Pos_bY+20, fill='white')
+#amplitude mouvement balle
+dbx = 4
+dby = 2
+
+#appelle fonction
+movement_balle()
+movement_raquette()
+pong.mainloop()
